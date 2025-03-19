89c77bd05f54696978e2d1b264a928391bb01320
David Garzon <juanxavif@gmail.com>
2025-03-19 17:05:23 -0500

    editando el header para que este mas estilizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <header className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 shadow-md z-50">
-            <nav className="fixed top-0 w-full bg-white shadow-md p-4">
-                <ul className="flex space-x-4 justify-center">
-                    <li><a href="#about" className="text-gray-400">Sobre mí</a></li>
-                    <li><a href="#projects" className="text-gray-400">Proyectos</a></li>
-                    <li><a href="#contact" className="text-gray-400">Contacto</a></li>
+        <header className="fixed top-0 left-0 w-full  bg-opacity-90 backdrop-blur-md shadow-md z-80 h-10 justify-center">
+            <nav className="p-4">
+                <ul className="flex space-x-8 justify-center">
+                    <li><a href="#about" className="hover:text-white transition">Sobre mí</a></li>
+                    <li><a href="#projects" className="hover:text-white transition">Proyectos</a></li>
+                    <li><a href="#contact" className="hover:text-white transition">Contacto</a></li>
                 </ul>
             </nav>
         </header>
-
-    )
+    );
 }
+
 
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <BrowserRouter>
     <App />
   </BrowserRouter>
