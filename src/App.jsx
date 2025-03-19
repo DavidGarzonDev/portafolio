95c535bfedb9e1bee66fbcbd788e45e8ccda6197
David Garzon <juanxavif@gmail.com>
2025-03-18 21:04:49 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Juan David</h1>
+      <p className="text-lg mt-2">Desarrollador en formación</p>
+    </div>
+  )
+}
+
+export default App
